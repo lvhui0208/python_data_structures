afd0f12e1227bc26603878b9f8b68eea302e23c5
lvhui0208 <1336753749@qq.com>
2020-03-04 10:16:03 +0800

    基本数据结构，pythonds和()匹配实现

--- ds/listPractise1.py
+++ ds/listPractise1.py
@@ -1,27 +1,30 @@
-def list1():
-    lsit1 = []
+# ??
+def test1():
+    list1 = []
     for i in range(1000):
         list1 = list1 + [i]
 
-
-#append方式
+# append??
 def test2():
     list1 = []
     for i in range(1000):
         list1.append(i)
 
+# ??????
 def test3():
-    list1 = [i  for i in range(1000)]:
-
-
+    list1 = [i for i in range(1000)]
 
 
 def test4():
     list1 = list(range(1000))
 
+from timeit import Timer
 
-import timeit
-
-t1 = timeit.Timer('test1()','form_main_import test1')
-paint(t1.timeit(number = 1000))
-
+t1 = Timer('test1()','from __main__ import test1')
+print('contact',t1.timeit(number=1000),'??')
+t2 = Timer("test2()", "from __main__ import test2")
+print("append",t2.timeit(number=1000), "??")
+t3 = Timer("test3()", "from __main__ import test3")
+print("comprehension ",t3.timeit(number=1000), "??")
+t4 = Timer("test4()", "from __main__ import test4")
+print("list range ",t4.timeit(number=1000), "??")
