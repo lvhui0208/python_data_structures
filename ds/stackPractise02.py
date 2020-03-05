98145b8f9ea88bf687f85f95b78f6d57936421aa
lvhui0208 <1336753749@qq.com>
2020-03-05 10:46:25 +0800

    队列模拟打印机完成

--- ds/stackPractise02.py
+++ ds/stackPractise02.py
@@ -1,122 +1,45 @@
-# 栈抽象数据类型的底层实现采用什么？   list
-# 确定列表的哪一端是顶部，然后使用append和pop来实现操作
-
-# 假设列表的尾部是栈的顶部元素，push
-# class Stack:
-#     def __init__(self):
-#         self.items = []
-
-#     def isEmpty(self):
-#         return self.items == []
-    
-#     def push(self,item):
-#         self.items.append(item)
-
-#     def pop(self):
-#         return self.items.pop()
-    
-#     def peek(self):
-#         return self.items[len(self.items)-1]
-    
-#     def size(self):
-#         return len(self.items)
-
-
-
-
+'''
+使用"除2"算法，将输入的十进制数字转换成8位2进制数字
+'''
 # from pythonds.basic.stack import Stack
 
-# s = Stack()
-
-# print(s.isEmpty())
-# s.push(4)
-# s.push('lalla')
-
-# print(s.peek())
-
-# s.push(True)
-
-# print(s.size())
-
-# print(s.isEmpty())
-
-# s.push(10.9)
-# print(s.pop())
-# print(s.pop())
-# print(s.size())
-
-
-
-
-# # （）匹配
-# from pythonds.basic.stack import Stack
-
-# #  symbolString  假设 “（（））”
-# def parChecker(symbolString):
+# def divide2(desNumber):
 #     s = Stack()
-#     flag = True
-#     index = 0
 
-#     while index < len(symbolString) and flag:
-#         symbol = symbolString[index]
-#         if symbol == "(":
-#             s.push(symbol)
-#         else:
-#             if s.isEmpty():
-#                 flag = False
-#             else:
-#                 s.pop()
+#     while desNumber > 0:
+#         rem = desNumber % 2
+#         s.push(rem)
+#         desNumber = desNumber//2
+    
+#     binString = ""
+#     while not s.isEmpty():
+#         binString = binString + str(s.pop())
+    
+#     return binString
 
-#         index = index + 1
-
-#     if flag and s.isEmpty():
-#         return True
-#     else:
-#         return False
+# print(divide2(233))
 
 
-# print(parChecker('(())'))   # 栈：  
-# print(parChecker('((())')) # 栈：（
-
-
-
-
-
-#  {[()]}   ( [ )]
-# 每一个开始的符号被压入栈，等待匹配结果
-# 当出现结束符号的时候，必须检查栈顶部的开始符号是什么类型，如果两个符号不匹配，则字符串不匹配
-# 整个字符串处理完并且栈为空，则字符串匹配
-
+'''
+八进制，十六进制
+'''
 from pythonds.basic.stack import Stack
 
-def parChecker(symbolString):
+def divideBase(desNumber,base):
+    digits = '0123456789ABCDEF'
     s = Stack()
-    flag = True
-    index = 0
 
-    while index < len(symbolString) and flag:
-        symbol = symbolString[index]
+    while desNumber > 0:
+        rem = desNumber % base
+        s.push(rem)
+        desNumber = desNumber // base
+    
+    binString = ""
+    while not s.isEmpty():
+        binString = binString + digits[s.pop()]
+    
+    return binString
 
-        if symbol in "([{":     # [{
-            s.push(symbol)
-        else:
-            if s.isEmpty():
-                flag = False
-            else:
-                top = s.pop()   # {
-                start = '([{'  # 2
-                end = ')]}'   # 1
-
-                if not start.index(top) == end.index(symbol):
-                    flag = False
-        index = index + 1 
-
-
-    if flag and s.isEmpty():
-        return True
-    else:
-        return False
-
-print(parChecker('{[()]}'))
-print(parChecker('{{([][])}()}'))
-print(parChecker('[{()]'))
+print(divideBase(233,2))
+print(divideBase(233,8))
+print(divideBase(233,16))
