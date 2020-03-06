00bbccbcb406a2566096c200b0d704e75bc159a2
lvhui0208 <1336753749@qq.com>
2020-03-06 16:48:06 +0800

    递归栈实现，可视化递归，汉诺塔游戏

--- ds2_recursion/listsum.py
+++ ds2_recursion/listsum.py
@@ -29,5 +29,55 @@ def toStr(n,base):
 
 print(toStr(1453,16))
 
+#栈 实现递归
+
+from pythonds.basic.stack import Stack
+
+rStack = Stack()
+
+def toStr1(n,base):
+    convertString = '0123456789ABCDEF'
+
+
+    while n>0:
+        if n < base:
+            rStack.push(convertString[n])
+        else:
+            rStack.push(convertString[ n % base ])
+
+
+        n = n//base
+    res = ''
+    while not rStack.isEmpty():
+        res = res + str(rStack.pop())
+    return res
+
+print(toStr1(1453,16))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
